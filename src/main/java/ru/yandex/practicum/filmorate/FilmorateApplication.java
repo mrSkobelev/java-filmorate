4beaf0c2e18269711303142a04839f89fb49e112
Alex <lelik-ttk@yandex.ru>
2023-10-30 22:29:08 +0300

    create film user FilmController UserController v2

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
-import java.time.LocalDate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.model.User;
 
 @SpringBootApplication
 public class FilmorateApplication {
